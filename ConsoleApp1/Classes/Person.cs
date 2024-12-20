f64f6ba7eb07e849240acf485db23f1e177b9fb6
andhiman590@outlook.com <andhiman590@outlook.com>
2024-12-20 19:58:05 +0530

    classes And Objects

--- ConsoleApp1/Classes/Person.cs
+++ ConsoleApp1/Classes/Person.cs
@@ -3,11 +3,142 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace ConsoleApp1.Classes
 {
+    // this is a class person where we can have multiple members 
     internal class Person
     {
 
+        public class Human
+        {
+
+            public int Id { get; set; }
+            public string? Name { get; set; }
+            public string? age { get; set; }
+
+            public void greet()
+            {
+                Console.WriteLine($"my name is {Name} , i am {age} years old . my identity is {Id}");
+            }
+        }
+
+        public class Members
+        {
+            //a class can have multiple members 
+            //Typically, fields are private to follow the principle of encapsulation,
+            //which hides implementation details from the outside.
+            private string? speed;   //fields 
+            private string? name;    //fields
+
+            //constructor to initalize a class
+            public Members(string speed, string name)
+            {
+                Speed = speed;
+                Name = name;
+            }
+            //properties to get and set the fields  -> member 2
+            public string Speed
+            {
+                get { return speed; }
+                set { speed = value; }
+            }
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+
+            public void greetmycar()
+            {
+                Console.WriteLine($"my car is {name} , i am {speed} ");
+            }
+
+        }
+
+        //access modifiers
+        //- > public : accessible everywhere
+        public class ModifierCheck
+        {
+            public int Id { get; set; }
+            public string? Name { get; set; }
+            public void publicCheck()
+            {
+                Console.WriteLine($"i am inevitable {Name} find my id at {Id}");
+            }
+
+        }
+        // -> private : accessible only within the class 
+        public class PrivateCheck
+        {
+            private int Id { get; set; }
+            private string? Name { get; set; }
+
+            private void privateCheck()
+            {
+                Console.WriteLine($"i am inevitable but i am private {Name} find my id at {Id}");
+            }
+
+            public void resultFromPrivate(int id, string name)
+            {
+                PrivateCheck values = new()
+                {
+                    Id = id,
+                    Name = name
+                };
+                privateCheck();
+            }
+        }
+
+        /*
+         Protected introduces a derived class . 
+         this concept is used in inheritence 
+         */
+        public class ProtectedCheck
+        {
+
+            protected string? Name { get; set; }
+            protected string? forInternalProtected { get; set; }
+
+            public  void setName(string name)
+            {
+                Name = name;
+            }
+            // this is for checking protected internal .
+            protected internal void setInternalString(string name)
+            {
+                forInternalProtected = name;
+                Console.WriteLine($"let us check for internal protected method {forInternalProtected } is the name ");
+            }
+
+            // this is for checking private protected 
+
+            private protected void getProtectedString(string name)
+            {
+                Console.WriteLine($"we will check for the private protected {name}");
+            }
+
+        }
+
+        public class InheritProtectedCheck : ProtectedCheck
+        {
+            public void showName()
+            {
+                //accesible as derived from the derived class . any member can be inherited 
+                Console.WriteLine($"my name is real slim shady {Name}");
+            }
+
+            // called protected internal method from the class derived 
+            public void showForinternal()
+            {
+                setInternalString("ANKIT PROTECTED INTERNAL STRING PROTECTED");
+            }
+            // derived class for a private protected getprotected string 
+            public void showforprotected()
+            {
+                getProtectedString("this is get protected string");
+            }
+        }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1.Classes;
+
 Console.WriteLine("Hello, World!");
+
+/*
+    now i have created a class that means i have a structure of a person
+    object is basically the instance of that class .
+    which means person1 is an object and neha is another object using the same structure to define their 
+    properties .
+*/
+Person.Human person1 = new();
+person1.Name = "ankit";
+person1.age = "27";
+person1.Id = 1;
+
+// or this can be written as 
+Person.Human Neha = new()
+{
+    Name = "Neha",
+    age = "29",
+    Id = 2,
+};
+Person.Members aloto = new Person.Members("120 Km/h", "Alto K10");
+person1.greet();
+Neha.greet();
+aloto.greetmycar();
+
+//checking access modifiers 
+//over here name and id are accesisble as they are public so is the method.
+Person.ModifierCheck modifierCheck = new()
+{
+    Id = 1,
+    Name = "Ankit dhiman"
+};
+modifierCheck.publicCheck();
+
+//checking nested private logic
+// outer classes cannot be private . 
+//nestedPrivate classes can be private
+Person.PrivateCheck nestedPrivate = new();
+nestedPrivate.resultFromPrivate(2,"Baanke Bihari");
+
+//inheritence and using protected classes with derived classes
+Person.InheritProtectedCheck inheritanceCheck = new();
+inheritanceCheck.setName("Slim Shady");
+inheritanceCheck.showName();
+
+// internal will be accessible across the same assembly or project .
+
+// protected internal -> same assembly and derived classes
+
+inheritanceCheck.showForinternal();
+// private internal -> within the class / derived class in the same assembly .
+inheritanceCheck.showforprotected();
